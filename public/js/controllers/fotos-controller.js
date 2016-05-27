9b7ec34e75b6dfc4ce40464948a6cf6ea3f2b6e5
declau <dec82@hotmail.com>
2016-05-27 16:09:11 -0300

    module 2

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,9 +3,9 @@ angular.module('decapp').controller('FotosController', function($scope, $http) {
     $scope.fotos = [];
 
     $http.get('/v1/fotos')
-    .success(function(retorno) {
-        console.log(retorno);
-        $scope.fotos = retorno; // não precisa fazer retorno.data
+    .success(function(fotos) {
+        console.log(fotos);
+        $scope.fotos = fotos; // não precisa fazer retorno.data
     })
     .error(function(erro) {
         console.log(erro);
